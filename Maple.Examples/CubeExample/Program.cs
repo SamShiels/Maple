beff9d1fc0c53c04aea33f6d04b1a442636d4d1e
Sam Shiels <sam.shiels@gmail.com>
2023-08-09 21:29:37 +0200

    Moved examples

---
+++ Maple.Examples/CubeExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CubeExample
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      using (Example example = new Example(1920, 1080, "Game"))
+      {
+        example.Run();
+      }
+    }
+  }
+}
